183121e1745f3708db31703fcd26f6329b6a9fb5
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-21 00:38:35 -0700

    added index app

---
+++ index.js
@@ -0,0 +1,15 @@
+// Main point of the appliction
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App Setup
+
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Sever listerniing on: ', port);
