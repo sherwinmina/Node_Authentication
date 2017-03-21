57cd6ffe70458fa76744642ed77b209f597813d2
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-21 13:18:27 -0700

    created services folder

---
+++ services/passport.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+
+// Setup options for JWT Startegy
+const jwtOptions = {
+  jwtFormRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.secret
+
+};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+
+  User.findByID(payload.sub, function(err, user) {
+
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+// Tell passport to use this strategy
