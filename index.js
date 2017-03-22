306b32a189d59f4ab851360be6ad03f6c32776fb
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-22 11:32:47 -0700

    import cors on index

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router')
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:auth/auth')
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:auth/auth')
 
 // App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({type: '*/*'}));
 router(app);
 
